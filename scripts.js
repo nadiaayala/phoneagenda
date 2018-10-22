93a05937e32d94851eda87a3361dcd26eb2b0055
Nadia Ayala <nadiaayalanogueira@outlook.com>
2018-10-22 10:59:49 -0300

    new feature: contacts will be displayed in alphabetical order.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,75 @@
 $(function(){
     const $btnAdd = $('#add');
     const $contacts = $('#contacts');
+    var $inputName = $('#name');
+    var $inputPhone = $('#phone');
     var $textEnteredName;
     var $textEnteredPhone;
+    var $divAlert;
+    const $letterA = $('#letterA');
+    const $letterB = $('#letterB');
+    const $letterC = $('#letterC');
+    const $letterD = $('#letterD');
+    const $letterE = $('#letterE');
+    const $letterF = $('#letterF');
+    const $letterG = $('#letterG');
+    const $letterH = $('#letterH');    
 
-    $('#name').on('keyup', function(){
+    $($inputName).on('keyup', function(){
         $textEnteredName= $('#name').val();
     });
 
-    $('#phone').on('keyup', function(){
+    $($inputPhone).on('keyup', function(){
         $textEnteredPhone= $('#phone').val();
     });
 
     $btnAdd.on('click', function(){ 
-        if($textEnteredName!='' && $textEnteredName!=undefined){
-            var $newContact = $('<div class="d-flex justify-content-around">');
-            var $newList = $('<div class="d-flex flex-row bg-light border border-dark w-75 justify-content-around mt-1"></div>')
-            var $newName = $('<div>' + $textEnteredName+' </div>');
-            var $newPhone = $('<div>'+ $textEnteredPhone+ ' </div>');
-            $newList.prepend($newName);
-            $newList.append($newPhone);
-            $newContact.append($newList);
-            $contacts.append($newContact);                      
+        if($textEnteredName!='' && $textEnteredName!=undefined)
+        {            
+            if($.isNumeric($textEnteredPhone)){
+                var $newContact = $('<div class="d-flex justify-content-around">');
+                var $newList = $('<div class="d-flex flex-row bg-light border border-dark w-75 justify-content-around mt-1"></div>')
+                var $newName = $('<div>' + $textEnteredName+' </div>');
+                var $newPhone = $('<div>'+ $textEnteredPhone+ ' </div>');
+                $newList.prepend($newName);
+                $newList.append($newPhone);
+                $newContact.append($newList);
+                var $first = $textEnteredName.charAt(0);
+                if( $first=='a'){ 
+                    $letterA.append($newContact);          
+                }
+                else if ($first=='b'){
+                    $letterB.append($newContact);
+                }
+                else if ($first=='c'){
+                    $letterC.append($newContact);
+                }           
+                else if ($first=='d'){
+                    $letterD.append($newContact);
+                }
+                else if ($first=='e'){
+                    $letterE.append($newContact);
+                }
+                else if ($first=='f'){
+                    $letterF.append($newContact);
+                }
+                else if ($first=='g'){
+                    $letterG.append($newContact);
+                }
+                else if ($first=='h'){
+                    $letterH.append($newContact);
+                }
+            }
+            else
+            {
+             $divAlert = $('<div class="alert alert-warning alert-dismissible"> Please, insert a valid phone number!</div>');
+             var $alertBtn = $('<button id="$closeBtn" class="close" data-dismiss="alert"> x </button>') 
+             $divAlert.append($alertBtn);
+             $contacts.prepend($divAlert);
+             $divAlert.on('click', function(){
+                 $(this).remove();
+            });
+            };
         }
                 
     });
